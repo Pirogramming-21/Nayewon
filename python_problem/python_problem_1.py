2b5bb434768d3857092848b5c51bcc3713fe4dca
yewonNa <nyw2352@gmail.com>
2024-07-05 22:58:11 +0900

    python_problem_1_7단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,33 +1,42 @@
 num = 0
 
 while num < 31:
-    while True:  
+    while True:
         try:
-            count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))  
-            if count not in [1, 2, 3]:  
-                print("1, 2, 3 중 하나를 입력하세요")  
+            count = int(input("부를 숫자의 개수를 입력하세요 (1, 2, 3만 입력 가능): "))
+            if count not in [1, 2, 3]:
+                print("1, 2, 3 중 하나를 입력하세요")
             else:
-                for i in range(1, count + 1):  
+                for i in range(count):
                     num += 1
-                    print(f"playerA : {num}")  
+                    print(f"playerA : {num}")
+                    if num >= 31:
+                        print("playerB win!")
+                        break
                 break
-        except ValueError:  
+        except ValueError:
             print("정수를 입력하세요")
+    if num >= 31:
+        break
 
-    while True:  
+    while True:
         try:
-            count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))  
-            if count not in [1, 2, 3]:  
-                print("1, 2, 3 중 하나를 입력하세요")  
+            count = int(input("부를 숫자의 개수를 입력하세요 (1, 2, 3만 입력 가능): "))
+            if count not in [1, 2, 3]:
+                print("1, 2, 3 중 하나를 입력하세요")
             else:
-                for i in range(1, count + 1):  
+                for i in range(count):
                     num += 1
-                    print(f"playerB : {num}") 
+                    print(f"playerB : {num}")
+                    if num >= 31:
+                        print("playerA win!")
+                        break
                 break
-        except ValueError:  
+        except ValueError:
             print("정수를 입력하세요")
-    if num >= 31:  
-            break  
-    
+    if num >= 31:
+        break
 
+# 창 바로 꺼지지 않게 코드 하나 추가했습니다. 
+input("")
 
