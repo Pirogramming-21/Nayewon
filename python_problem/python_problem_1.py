fb0956f7683c8679ca8fbcaabd43cb5ec69a97d4
yewonNa <nyw2352@gmail.com>
2024-07-05 23:00:14 +0900

    python_problem_1_8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,40 +1,30 @@
+def brGame(player):
+    global num  # 전역 변수 num을 사용하기 위해 global 선언
+    while True:  
+        try:
+            count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))  
+            if count not in [1, 2, 3]:  
+                print("1, 2, 3 중 하나를 입력하세요")  
+            else:
+                for i in range(1, count + 1):  
+                    num += 1
+                    print(f"{player} : {num}") 
+                    if num >= 31:  
+                        if player == "playerA":
+                            print("playerB win!")
+                        else:
+                            print("playerA win!")
+                        return True
+                return False
+        except ValueError:  
+            print("정수를 입력하세요")
+
 num = 0
 
 while num < 31:
-    while True:
-        try:
-            count = int(input("부를 숫자의 개수를 입력하세요 (1, 2, 3만 입력 가능): "))
-            if count not in [1, 2, 3]:
-                print("1, 2, 3 중 하나를 입력하세요")
-            else:
-                for i in range(count):
-                    num += 1
-                    print(f"playerA : {num}")
-                    if num >= 31:
-                        print("playerB win!")
-                        break
-                break
-        except ValueError:
-            print("정수를 입력하세요")
-    if num >= 31:
+    if brGame("playerA"):
         break
-
-    while True:
-        try:
-            count = int(input("부를 숫자의 개수를 입력하세요 (1, 2, 3만 입력 가능): "))
-            if count not in [1, 2, 3]:
-                print("1, 2, 3 중 하나를 입력하세요")
-            else:
-                for i in range(count):
-                    num += 1
-                    print(f"playerB : {num}")
-                    if num >= 31:
-                        print("playerA win!")
-                        break
-                break
-        except ValueError:
-            print("정수를 입력하세요")
-    if num >= 31:
+    if brGame("playerB"):
         break
 
 # 창 바로 꺼지지 않게 코드 하나 추가했습니다. 
