e57692882d26449615bdffabcbdf1e970c35b8d2
yewonNa <nyw2352@gmail.com>
2024-07-05 23:01:38 +0900

    python_problem_1_9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,30 +1,37 @@
+import random
+
 def brGame(player):
     global num  # 전역 변수 num을 사용하기 위해 global 선언
-    while True:  
-        try:
-            count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))  
-            if count not in [1, 2, 3]:  
-                print("1, 2, 3 중 하나를 입력하세요")  
+    if player == "computer":
+        count = random.randint(1, 3)
+    else:
+        while True:  
+            try:
+                count = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))  
+                if count not in [1, 2, 3]:  
+                    print("1, 2, 3 중 하나를 입력하세요")  
+                else:
+                    break
+            except ValueError:  
+                print("정수를 입력하세요")
+
+    for i in range(1, count + 1):  
+        num += 1
+        print(f"{player} : {num}") 
+        if num >= 31:  
+            if player == "player":
+                print("computer win!")
             else:
-                for i in range(1, count + 1):  
-                    num += 1
-                    print(f"{player} : {num}") 
-                    if num >= 31:  
-                        if player == "playerA":
-                            print("playerB win!")
-                        else:
-                            print("playerA win!")
-                        return True
-                return False
-        except ValueError:  
-            print("정수를 입력하세요")
+                print("player win!")
+            return True
+    return False
 
 num = 0
 
 while num < 31:
-    if brGame("playerA"):
+    if brGame("computer"):
         break
-    if brGame("playerB"):
+    if brGame("player"):
         break
 
 # 창 바로 꺼지지 않게 코드 하나 추가했습니다. 
